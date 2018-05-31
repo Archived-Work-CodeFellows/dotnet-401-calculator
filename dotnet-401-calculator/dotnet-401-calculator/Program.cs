e8acf4b7d5a0c96ac78b632fc3d0f6b6676d0baa
Eric Singleton <eric.singleton.jr@gmail.com>
2018-05-31 10:38:06 -0700

    Added some simple math functions

--- dotnet-401-calculator/dotnet-401-calculator/Program.cs
+++ dotnet-401-calculator/dotnet-401-calculator/Program.cs
@@ -6,7 +6,35 @@ namespace dotnet_401_calculator
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Intro();
+
+        }
+        private static void Intro()
+        {
+            Console.WriteLine("Welcome to the awesomest Calculator because math");
+            Console.Write("\n\n");
+            Console.Write("Press enter to begin mathing...");
+            Console.Read();
+        }
+        private static void CalcMenu()
+        {
+
+        }
+        private static int Addition(int a, int b)
+        {
+            return a + b;
+        }
+        private static int Subtraction(int a, int b)
+        {
+            return a - b;
+        }
+        private static int Multiplication(int a, int b)
+        {
+            return a * b;
+        }
+        private static int Division(int a, int b)
+        {
+            return a / b;
         }
     }
 }
