3966c94e9753513acc8c2805b91319bd1976a24c
Eric Singleton <eric.singleton.jr@gmail.com>
2018-05-31 12:06:23 -0700

    Added some menu interactions and the base operations

--- dotnet-401-calculator/dotnet-401-calculator/Program.cs
+++ dotnet-401-calculator/dotnet-401-calculator/Program.cs
@@ -6,20 +6,33 @@ namespace dotnet_401_calculator
     {
         static void Main(string[] args)
         {
+            double[] variables = new double[2];
             Intro();
-
+            VariableSelector(variables);
+            Console.Write($"you wrote {variables[0]} and {variables[1]} for your variables");
+            Console.ReadLine();
         }
+
+        //***** Menu Interactions
         private static void Intro()
         {
             Console.WriteLine("Welcome to the awesomest Calculator because math");
             Console.Write("\n\n");
             Console.Write("Press enter to begin mathing...");
-            Console.Read();
+            Console.ReadLine();
         }
-        private static void CalcMenu()
+        private static double[] VariableSelector(double[] variables)
         {
-
+            Console.Clear();
+            Console.Write("Type in your first number: ");
+            variables[0] = Int32.Parse(Console.ReadLine());
+            Console.Clear();
+            Console.Write("Now give a second number: ");
+            variables[1] = Int32.Parse(Console.ReadLine());
+            return variables;
         }
+
+        //***** Math operations
         private static int Addition(int a, int b)
         {
             return a + b;
