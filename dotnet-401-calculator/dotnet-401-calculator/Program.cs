d9fbfe976d321efba8c895c4fe10e42397b3c398
Eric Singleton <eric.singleton.jr@gmail.com>
2018-06-02 23:02:40 -0700

    Added simple exponential and modulo, also some more looping to catch some invalid inputs

--- dotnet-401-calculator/dotnet-401-calculator/Program.cs
+++ dotnet-401-calculator/dotnet-401-calculator/Program.cs
@@ -8,19 +8,30 @@ namespace dotnet_401_calculator
         {
             double[] variables = new double[2];
             bool confirm = true;
+            bool active = true;
+            bool rerun = false;
+
             Intro();
 
             do
             {
-                VariableSelector(variables);
-                confirm = ConfirmVar(variables);
+                do
+                {
+                    VariableSelector(variables);
+                    confirm = ConfirmVar(variables);
 
-            } while (confirm);
-            int operation = OperandSelector();
-            Console.WriteLine($"You Picked {operation}");
-            Calculations(operation, variables);
+                } while (confirm);
+                do
+                {
+                    int operation = OperandSelector();
+                    Console.WriteLine($"You Picked {operation}");
+                    rerun = Calculations(operation, variables);
 
-            Console.ReadLine();
+                } while (rerun);
+
+                active = AppContinue();
+
+            } while (active);
         }
 
         //***** Menu Interactions
@@ -28,6 +39,8 @@ namespace dotnet_401_calculator
         {
             Console.WriteLine("Welcome to the awesomest Calculator because math");
             Console.Write("\n\n");
+            Console.WriteLine("Think about the operation you would like to proceed with when selecting numbers");
+            Console.Write("\n");
             Console.Write("Press enter to begin mathing...");
             Console.ReadLine();
         }
@@ -50,33 +63,50 @@ namespace dotnet_401_calculator
             Console.WriteLine("2) subtraction");
             Console.WriteLine("3) multiplication");
             Console.WriteLine("4) division");
+            Console.WriteLine("5) power");
+            Console.WriteLine("6) modulo/remainder");
             Console.Write("\n\n");
             Console.Write("Please select a option... ");
             return Int32.Parse(Console.ReadLine());
 
         }
-        private static void Calculations(int operation, double[] variables)
+        private static bool Calculations(int operation, double[] variables)
         {
             double result;
-            switch(operation)
+            bool rerun = false;
+            switch (operation)
             {
                 case 1:
                     result = Addition(variables[0], variables[1]);
-                    Console.WriteLine($"After adding them together, we got {result}");
+                    Console.WriteLine($"Welcome to the summation nation. {result} is your answer.");
                     break;
                 case 2:
                     result = Subtraction(variables[0], variables[1]);
-                    Console.WriteLine($"After subtracting them from another, we got {result}");
+                    Console.WriteLine($"Subtract that. {result} is the reasonable response.");
                     break;
                 case 3:
                     result = Multiplication(variables[0], variables[1]);
-                    Console.WriteLine($"After multiplying them together, we got {result}");
+                    Console.WriteLine($"Multiplication situation. {result} is your answer.");
                     break;
                 case 4:
                     result = Division(variables[0], variables[1]);
-                    Console.WriteLine($"After dividing them from another, we got {result}");
+                    Console.WriteLine($"After dividing them from another, we got {result}. No catchy thing here.");
+                    break;
+                case 5:
+                    result = Math.Pow(variables[0], variables[1]);
+                    Console.WriteLine($"After summoning the power of greyskull, we exponentially got {result}");
+                    break;
+                case 6:
+                    result = Remainder(variables[0], variables[1]);
+                    Console.WriteLine($"I'm here to give you a remainder. {result} is left");
+                    break;
+                default:
+                    Console.WriteLine($"Sorry but {operation} isn't a valid choice");
+                    rerun = true;
                     break;
             }
+            Console.ReadLine();
+            return rerun;
         }
         //***** User Confirmation to either keep or re-run the variable selection
         private static bool ConfirmVar(double[] variables)
@@ -87,6 +117,14 @@ namespace dotnet_401_calculator
             Console.Write("Do you want to keep these variables? type y/n: ");
             string input = Console.ReadLine();
             return input == "n" ? true : false;
+        }
+        private static bool AppContinue()
+        {
+            Console.Clear();
+            Console.Write("Would you like to continue the math? y/n: ");
+            string input = Console.ReadLine().ToLower();
+            if (input == "y") return true;
+            else return false;
         }
 
         //***** Math operations
@@ -105,6 +143,10 @@ namespace dotnet_401_calculator
         private static double Division(double a, double b)
         {
             return a / b;
+        }
+        private static double Remainder(double a, double b)
+        {
+            return a % b;
         }
     }
 }
